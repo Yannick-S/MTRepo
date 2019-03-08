8cc51c407ce952028ecb34825c22c58172aa8d1e
Yannick S <saive@kth.se>
2019-03-08 11:15:09 +0100

    actaully works now

--- repo/utility/checknotebook.py
+++ repo/utility/checknotebook.py
@@ -1,9 +1,12 @@
 def in_ipynb():
     try:
         cfg = get_ipython().config 
-        if cfg['IPKernelApp']['parent_appname'] == 'ipython-notebook':
+        if 'jupyter' in cfg['IPKernelApp']['connection_file']:
             return True
         else:
             return False
     except NameError:
         return False
+    
+if __name__ == "__main__":
+    print(in_ipynb())
