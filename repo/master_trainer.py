a99d902f81c7013e676e0ef252b77e7cb2ac615e
Yannick S <saive@kth.se>
2019-03-27 16:20:05 +0100

    move else load over

--- repo/master_trainer.py
+++ repo/master_trainer.py
@@ -11,22 +11,11 @@ optimizer = model.get_optimizer()
 loss = torch.nn.NLLLoss()
 
 #### load model
+from model_loader import load_model, else_load
 if load_from_file:
-    from model_loader import load_model
-    model, optimizer, training_history, start_epoch, path = load_model(
-        model_info,
-        model
-    )
+    model, optimizer, training_history, start_epoch, path = load_model(model_info,model)
 else:
-    training_history = {'nll': [], 'acc': []}
-    import datetime, os
-    now = datetime.datetime.now()
-    path = 'checkpoint/' + model_info["name"]
-    path = path +'/{:04d}-{:02d}-{:02d}'.format(now.year, now.month , now.day)
-    path = path + '_{:02d}:{:02d}:{:02d}/'.format(now.hour, now.minute, now.second)
-    if not os.path.isdir('checkpoint/'): os.mkdir('checkpoint')
-    if not os.path.isdir('checkpoint/' + model_info["name"]): os.mkdir('checkpoint/' + model_info["name"])
-    os.mkdir(path)
+    training_history, path = else_load(model_info)
 
 #### load data
 from data_loader import data_from_data_info
@@ -35,15 +24,4 @@ train_loader, val_loader = data_from_data_info(model_info["data"])
 #### train
 from ignite_train import run
 
-
-
-run(model, 
-    optimizer,
-    loss, 
-    device, 
-    train_loader, 
-    training_history,
-    model_info,
-    start_epoch,
-    path
-    )
+run(model, optimizer, loss, device, train_loader, training_history, model_info, start_epoch, path)
--- repo/model_loader.py
+++ repo/model_loader.py
@@ -4,9 +4,6 @@ import torch
 def load_model(model_info, model):
     name = model_info["name"]
 
-    # set up optimizer
-    if model_info[""]
-
     # find latest model
     all_dirs = os.listdir('checkpoint' + '/' + name)
     sorted_dirs = sorted(all_dirs)
@@ -25,3 +22,16 @@ def load_model(model_info, model):
     start_epoch = checkpoint['epoch']
 
     return model, optimizer, training_history, start_epoch, last_dir 
+
+def else_load(model_info):
+    training_history = {'nll': [], 'acc': []}
+    import datetime, os
+    now = datetime.datetime.now()
+    path = 'checkpoint/' + model_info["name"]
+    path = path +'/{:04d}-{:02d}-{:02d}'.format(now.year, now.month , now.day)
+    path = path + '_{:02d}:{:02d}:{:02d}/'.format(now.hour, now.minute, now.second)
+    if not os.path.isdir('checkpoint/'): os.mkdir('checkpoint')
+    if not os.path.isdir('checkpoint/' + model_info["name"]): os.mkdir('checkpoint/' + model_info["name"])
+    os.mkdir(path)
+
+    return training_history, path
