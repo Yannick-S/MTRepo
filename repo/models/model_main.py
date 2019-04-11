743d22416c97731578f1da68de56cc80dd5f38c2
Yannick S <saive@kth.se>
2019-04-11 12:04:40 +0200

    as

--- repo/models/model_main.py
+++ repo/models/model_main.py
@@ -24,8 +24,8 @@ class Net(torch.nn.Module):
         #data
         #self.data_name = "ModelNet10"
         self.data_name = "Geometry"
-        self.batch_size = 3
-        self.nr_points = 100
+        self.batch_size = 30
+        self.nr_points = 1024
         self.nr_classes = 10 if self.data_name == 'ModelNet10' else 40
 
         #train_info
